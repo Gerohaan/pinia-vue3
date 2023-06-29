917df958ab64c0030244a4d404b4a9df49aa7254
gerohaan <gero.delfin@gmail.com>
2023-06-29 19:42:09 -0400

    Practic

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <nav>
+      <RouterLink to="/">Home</RouterLink> | 
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <br>
+    <RouterView />
+  </div>
+</template>
+<style>
+.app {
+  text-align: center;
+}
+</style>
